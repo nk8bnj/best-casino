5284ec52021e2c646e2eec72f92f417dd8f75aa9
Nikita Ganzha <nk8bnj@gmail.com>
2026-01-09 09:15:44 +0200

    fix: move routs into const

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { Container, PageWrapper } from "@/components/layout";
 import Link from "next/link";
+import { ROUTES } from "@/config/routes";
 
 export default function Home() {
   return (
     <PageWrapper>
       <Container>
         <h1>Blaze Casino</h1>
-        <Link href="/login">Log in</Link>
+        <Link href={ROUTES.LOGIN}>Log in</Link>
       </Container>
     </PageWrapper>
   );
